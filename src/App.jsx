fd74b6bacbc5afafc75709b9a8bc9b71872145a1
falakheshabangu <falakheshangu@gmail.com>
2025-12-04 22:22:28 +0200

    Last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
+import { CORE_CONCEPTS, EXAMPLES } from './data'
+import Header from './components/Header/Header'
+import CoreConcept from './components/CoreConcept'
+import TabButton from './components/TabButton';
+import { useState } from 'react';
+
 function App() {
+
+  const [selectedToptic, setSelectedTopic] = useState()
+
+  const handleClick = (selectedButton)=> {
+        
+        setSelectedTopic(selectedButton) 
+
+
+    }
+
+    let tabContent = <p>Please select a topic</p>
+
+    if(selectedToptic){
+      tabContent = (
+        <div id='tab-content'>
+            <h3>{EXAMPLES[selectedToptic].title}</h3>
+            <p>{EXAMPLES[selectedToptic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedToptic].code}
+              </code>
+            </pre>
+          </div>
+      )
+    }
+
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map( (conceptItem)=> ( <CoreConcept key={conceptItem.title} {...conceptItem} /> ) )}
+          </ul>
+        </section>
+        <section id='examples'>
+          
+          <menu>
+            <TabButton handleClick={() => handleClick('components')} isSelected={selectedToptic === 'component'}>Component</TabButton>
+            <TabButton handleClick={() => handleClick('jsx')} isSelected={selectedToptic === 'jsx'}>JSX</TabButton>
+            <TabButton handleClick={() => handleClick('props')} isSelected={selectedToptic === 'props'}>Props</TabButton>
+            <TabButton handleClick={() => handleClick('state')} isSelected={selectedToptic === 'state'}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+export default function TabButton( {children, handleClick, isSelected} ) {
+
+    
+
+    return (
+        <li>
+            <button className={isSelected ? 'active' : undefined } onClick={handleClick} >{children}</button>
+        </li>
+    )
+}
